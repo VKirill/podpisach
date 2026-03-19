20fefb282187ce281610923cfeafc21d04c31d0f
VKirill <vechkasov@gmail.com>
2026-03-19 20:00:11 +0300

    fix: use Nuxt auto-import prefixed component names (SetupStepPassword etc.)

--- apps/web/components/setup/SetupWizard.vue
+++ apps/web/components/setup/SetupWizard.vue
@@ -4,9 +4,9 @@ const { currentStep } = useSetup()
 
 <template>
   <div>
-    <StepPassword v-if="currentStep === 1" />
-    <StepBot v-else-if="currentStep === 2" />
-    <StepChannel v-else-if="currentStep === 3" />
-    <StepComplete v-else-if="currentStep === 4" />
+    <SetupStepPassword v-if="currentStep === 1" />
+    <SetupStepBot v-else-if="currentStep === 2" />
+    <SetupStepChannel v-else-if="currentStep === 3" />
+    <SetupStepComplete v-else-if="currentStep === 4" />
   </div>
 </template>
